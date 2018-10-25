796c6664c98cbbc8c4872c2a3ea2e26f871ebff8
mcabral <mcabral@ascentio.com.ar>
2018-10-25 18:05:48 -0300

    remove from menu ext error pages  option

--- src/router/index.js
+++ src/router/index.js
@@ -154,31 +154,6 @@ export const asyncRouterMap = [
   },
 
   {
-    path: '/error',
-    component: Layout,
-    redirect: 'noredirect',
-    name: 'ErrorPages',
-    meta: {
-      title: 'errorPages',
-      icon: '404'
-    },
-    children: [
-      {
-        path: '401',
-        component: () => import('@/views/errorPage/401'),
-        name: 'Page401',
-        meta: { title: 'page401', noCache: true }
-      },
-      {
-        path: '404',
-        component: () => import('@/views/errorPage/404'),
-        name: 'Page404',
-        meta: { title: 'page404', noCache: true }
-      }
-    ]
-  },
-
-  {
     path: '/error-log',
     component: Layout,
     redirect: 'noredirect',
