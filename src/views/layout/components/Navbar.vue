e87eed51552f458c4ab6308a1b1044e6f42eb1c3
mcabral <mcabral@ascentio.com.ar>
2018-10-30 13:20:01 -0300

    borrado de componentes no utilizados

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -25,7 +25,7 @@
 
       <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src="avatar+'?imageView2/1/w/50/h/50'" class="user-avatar">
           <i class="el-icon-caret-bottom"/>
         </div>
         <el-dropdown-menu slot="dropdown">
